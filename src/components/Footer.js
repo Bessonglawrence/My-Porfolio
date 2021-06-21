73be4569a975809f5e76836fdc6b30bb164df994
Bessong lawrence <orockbessonglawrence@gmail.com>
2021-06-21 11:01:01 +0100

    Created Navbar, Footer and Hero Sections files

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () =>{
+    return(
+        <div>
+            <h3>This is the footer component</h3>
+        </div>
+    );
+}
+export default Footer;
---
+++ src/sections/Hero.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Hero = () =>{
+    return(
+        <div>
+            <h3> This is the Hero section</h3>
+        </div>
+    );
+}
+export default Hero;
